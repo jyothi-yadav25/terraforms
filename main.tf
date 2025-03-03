636c9e8f85eb48b54826e290a77213912d7de9ba
Jyothi Yadav <jyothi@Jyothis-MacBook-Pro.local>
2025-03-03 10:00:44 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,16 +1,37 @@
-
-
-
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo 'This is a simple null resource example in IBM Schematics!'"
-  }
-
-  triggers = {
-    example_trigger = "simple_trigger"
+terraform {
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "~> 1.55.0"
+    }
   }
 }
 
-output "null_resource_example_name" {
-  value = null_resource.example.id
+provider "ibm" {
+  region = "us-south"
+  ibmcloud_api_key = var.ibmcloud_api_key
+}
+variable "ibmcloud_api_key" {}
+
+
+resource "ibm_cd_toolchain" "cd_toolchain_instance" {
+  description = "A sample toolchain to test the API"
+  name = "TestToolchain"
+  resource_group = "default"
+  
+}
+
+resource "ibm_cd_toolchain_tool_githubconsolidated" "cd_toolchain_tool_githubconsolidated_instance" {
+  initialization {
+        git_id = "github"
+        owner_id = "jyothi-yadav25"
+        repo_name = "terraforms.git"
+        source_repo_url = "https://github.com/jyothi-yadav25/terraforms.git"
+        type = "clone"
+        
+  }
+  parameters {
+        
+  }
+  toolchain_id = ibm_cd_toolchain.cd_toolchain.id
 }
